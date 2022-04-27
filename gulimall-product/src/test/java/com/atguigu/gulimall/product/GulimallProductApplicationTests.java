c53fa618c8b9c5628a6a293191eae1b8556ba600
Your Name <you@example.com>
2022-04-27 18:00:11 +0800

    gulimall全栈开发篇漏洞解决一部分现在开始提交

---
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,27 @@
+package com.atguigu.gulimall.product;
+
+import com.atguigu.gulimall.product.service.BrandService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * 1、引入oss-starter
+ * 2、配置key，endpoint相关信息即可
+ * 3、使用OSSClient 进行相关操作
+ */
+@Slf4j
+@SpringBootTest
+public class GulimallProductApplicationTests {
+
+
+
+
+}
---
+++ gulimall-search/src/test/java/com/atguigu/gulimall/search/GulimallSearchApplicationTests.java
@@ -0,0 +1,22 @@
+package com.atguigu.gulimall.search;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallSearchApplicationTests {
+
+    @Autowired
+    private RestHighLevelClient client;
+
+    @Test
+    void contextLoads() {
+        System.out.println(client);
+
+    }
+}
+
+
